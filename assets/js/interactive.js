81f4061b14f5070c9f8ec4ab1facd951b88a4922
Arjun <arjunthakur01081997@gmail.com>
2017-08-17 01:42:02 +0530

    Added play/pause icon effect when user click on the video. (Enhancement- issue #3)

--- assets/js/interactive.js
+++ assets/js/interactive.js
@@ -1,10 +1,25 @@
 // Video Player - An open source project
 
 $(document).ready(function(){
+	$("#video, #playPause").click(function(){
+		if($("#video").get(0).paused){
+			$("#centerPlayPause").addClass("fa-pause");
+			$("#centerPlayPause").removeClass("fa-play");
+			$("#centerPlayPause").fadeIn(100).fadeOut(200);
+		}
+		else{
+			$("#centerPlayPause").addClass("fa-play");
+			$("#centerPlayPause").removeClass("fa-pause");
+			$("#centerPlayPause").fadeIn(100).fadeOut(200);
+		}
+	});
+
+/*
 	$("#videoContainer").keydown(function(event){ 
     	if(event.which=='32')
 		{
 			alert("Space bar is pressed!")
 		}
     });
+*/
 });
